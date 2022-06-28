fa1efcdfae17b4ff4f7d35c7792517d5979c1f1d
Davi Lacerda <dlacerda9@gmail.com>
2022-06-28 16:51:23 -0300

    Revert "fix: Refresh button"
    This reverts commit 5b7ff32ef0ae8c03a0f2738c0bafc61f3e5b37a2.

--- src/components/RefreshButton/index.tsx
+++ src/components/RefreshButton/index.tsx
@@ -3,17 +3,12 @@ import { useRouter } from 'next/router';
 
 export function RefreshButton(){
     const router = useRouter();
-
-    const handleRefresh = () => {
-        const url = router.asPath;
-        router.push(url);
-    }
     
     return (
         <button
             type="button"
             className="flex flex-row items-center gap-2 p-2 rounded-lg w-full max-w-[250px] justify-center bg-brand-default hover:brightness-75 transition-colors"
-            onClick={handleRefresh}
+            onClick={() => router.reload()}
         >
             <span className='text-black'>Try again!</span>
             <IoIosRefresh className="fill-black w-8 h-10" />
