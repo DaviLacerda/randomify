5e3b39ca66bb25c23555225346267b577015da19
Davi Lacerda <dlacerda9@gmail.com>
2022-10-15 20:06:45 -0300

    feat: add Home Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,20 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
+import Logo from "../components/Logo";
+import SearchInput from "../components/SearchInput";
 
 const Home: NextPage = () => {
-  return (
-    <h1>Hello Randomify</h1>
-  )
-}
+    return (
+        <div className="flex flex-col lg:flex-row items-center justify-center gap-8 min-h-screen p-4">
+            <aside className="flex items-left flex-col gap-3 max-w-sm">
+                <Logo />
+                <p className="font-semibold font-sans">
+                    Random Episodes Generator, for you to enjoy with fun but
+                    without knowing how!
+                </p>
+            </aside>
+            <SearchInput />
+        </div>
+    );
+};
 
-export default Home
+export default Home;
