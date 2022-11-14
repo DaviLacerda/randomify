3105b50e687489256642fec8c73e1a742654f769
Davi Lacerda <dlacerda9@gmail.com>
2022-11-14 20:11:15 -0300

    styles: Logo text size

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -2,9 +2,9 @@ import Image from "next/image"
 
 const Logo = () => {
     return (
-        <div className='flex items-center gap-2'>
+        <div className='flex items-center gap-2 w-full'>
             <Image src="/assets/logo.svg" alt="Logo" width={32} height={32} />
-            <span className='text-4xl font-bold text-brand-400'>Randomify</span>
+            <span className='text-2xl sm:text-4xl font-bold text-brand-400'>Randomify</span>
         </div>
     )
 }
