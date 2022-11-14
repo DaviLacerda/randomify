8cd45d3124d780f1937d54a510169ae7115f3541
Davi Lacerda <dlacerda9@gmail.com>
2022-11-14 00:39:42 -0300

    refactor: disabled button and handleRedirect

--- src/components/SearchInput/index.tsx
+++ src/components/SearchInput/index.tsx
@@ -2,8 +2,7 @@ import Turnstone from "turnstone";
 import { api, api_key } from "../../utils/api";
 import { AiOutlineArrowRight } from "react-icons/ai";
 import { useRouter } from "next/router";
-import Image from "next/image";
-import { FormEvent } from "react";
+import { FormEvent, useState } from "react";
 
 type Item = {
     item: {
@@ -13,39 +12,38 @@ type Item = {
     };
 };
 
+const styles = {
+    query: "placeholder:text-[#73737350]",
+    input: "bg-neutral-800 w-full max-w-sm h-12 p-4 transition-colors border-2 border-transparent duration-300",
+    typeahead: "text-neutral-600",
+    inputFocus:
+        "bg-neutral-800 w-full max-w-sm h-12 p-4 outline-none border-brand-400 border-2",
+    listbox: "bg-neutral-800 w-full max-w-sm",
+    noItems: "w-full h-16 px-4 py-1 bg-neutral-800 flex items-center",
+    highlightedItem: "bg-neutral-700",
+    clearButton:
+        "absolute w-10 h-12 inset-y-0 right-0 text-2xl items-center justify-center z-10 text-white inline-flex transition-colors hover:text-neutral-600",
+};
+
+const listbox = {
+    displayField: "name",
+    data: async (query: string) => {
+        const res = await api.get(
+            `search/tv?api_key=${api_key}&page=1&query=${query}&include_adult=false`
+        );
+        const { results } = res.data;
+        return results;
+    },
+    searchType: "contains",
+};
+
 const SearchInput = () => {
+    const [query, setQuery] = useState("");
     const router = useRouter();
-
-    const styles = {
-        query: "placeholder:text-[#73737350]",
-        input: "bg-neutral-800 w-full max-w-sm h-12 p-4 transition-colors border-2 border-transparent duration-300",
-        typeahead: "text-neutral-600",
-        inputFocus:
-            "bg-neutral-800 w-full max-w-sm h-12 p-4 outline-none border-brand-400 border-2",
-        listbox: "bg-neutral-800 w-full max-w-sm",
-        noItems: "w-full h-16 px-4 py-1 bg-neutral-800 flex items-center",
-        highlightedItem: "bg-neutral-700",
-        clearButton:
-            "absolute w-10 h-12 inset-y-0 right-0 text-2xl items-center justify-center z-10 text-white inline-flex transition-colors hover:text-neutral-600",
-    };
-
-    const listbox = {
-        displayField: "name",
-        data: async (query: string) => {
-            const res = await api.get(
-                `search/tv?api_key=${api_key}&page=1&query=${query}&include_adult=false`
-            );
-            const { results } = res.data;
-            return results;
-        },
-        searchType: "contains",
-    };
 
     const handleRedirect = (event: FormEvent) => {
         event.preventDefault();
-        const input = document.getElementById("search") as HTMLInputElement;
-        const filteredValue = input.value.replace(/\s/g, "-");
-        router.push(`/search/${filteredValue}`);
+        router.push(`/search/${query}`);
     };
 
     const Item = ({ item }: Item) => {
@@ -55,10 +53,10 @@ const SearchInput = () => {
                 <span className="font-semibold text-white">{name}</span>
                 { poster_path && (
                     <img
-                    src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-                    alt={name}
-                    className="h-full w-10"
-                />
+                        src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+                        alt={name}
+                        className="h-full w-10"
+                    />
                 ) }
             </div>
         );
@@ -78,13 +76,15 @@ const SearchInput = () => {
                         Item={Item}
                         maxItems={5}
                         debounceWait={500}
+                        onChange={(value:string) => setQuery(value)}
                     />
                 </div>
                 <button
+                    disabled={query === ""}
                     type="submit"
-                    className="flex items-center h-12 border-2 border-transparent p-2 focus:border-2 focus:border-brand-400 outline-none rounded transition-colors hover:bg-neutral-800"
+                    className="group flex items-center h-12 border-2 border-transparent p-2 focus:border-2 focus:border-brand-400 outline-none rounded transition-colors hover:bg-neutral-800 disabled:cursor-not-allowed"
                 >
-                    <AiOutlineArrowRight className="fill-brand-400 h-8 w-8 transition-colors duration-200" />
+                    <AiOutlineArrowRight className="fill-brand-400 h-8 w-8 transition-colors duration-200 group-disabled:opacity-50" />
                 </button>
             </form>
         </div>
